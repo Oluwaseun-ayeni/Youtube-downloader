2a7f5b73f311296f33cad0193724304e2378b8bf
Oluwaseun-ayenni <72kulbranking@gmail.coom>
2023-01-27 10:17:57 +0000

    changes

--- Backend/downloader/downloader/settings.py
+++ Backend/downloader/downloader/settings.py
@@ -114,8 +114,8 @@ DATABASES = {
         'NAME': env("DB_NAME"),
         'USER': env("DB_USER"),
         'PASSWORD': env("DB_PASSWORD"),
-        'HOST': env("DB_HOST"),
-        'PORT': env("DB_PORT"),
+        'HOST': "3.84.4.20",
+        'PORT': "",
     }
     
 }
